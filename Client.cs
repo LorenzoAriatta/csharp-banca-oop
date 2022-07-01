744366bda7faff3dd231cf3cfc4b47070899b5e3
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-01 14:39:41 +0200

    added base variable

---
+++ Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Client
+{
+	public string Name { get; private set; }
+	public string Surname { get; private set; }
+	public string CF { get; private set; }
+	public int Salary { get; private set; }
+	public Client(string name, string surname, string cf, int salary)
+	{
+		this.Name = name;
+		this.Surname = surname;
+		this.CF = cf;
+		this.Salary = salary;
+	}
+}
---
+++ Lending.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Lending
+{
+	public string ID { get; set; }
+
+	Client lender;
+	public int Installment { get; set; }
+	DateTime StartLend { get; set; }
+	DateTime EndLend { get; set; }
+
+	public Lending()
+	{
+	}
+}
