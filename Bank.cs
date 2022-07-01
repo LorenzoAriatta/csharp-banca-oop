5328e8e058573f51a6b94c637d66b303ba65088f
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-01 16:21:01 +0200

    add function to search and modify clients

--- Bank.cs
+++ Bank.cs
@@ -9,18 +9,54 @@ public class Bank
 	public Bank(string name)
 	{
 		this.Name = name;
+
+		clients = new List<Client>();
+		lendings = new List<Lending>();
+
+		Client user = new Client("Lorenzo", "Ariatta", "qwe123", 5000);
+		Client user2 = new Client("Stefano", "Calzoni", "qwe321", 5000);
+		clients.Add(user);
+		clients.Add(user2);
+
 	}
 
-	public Client NewClient()
+
+	internal Client SearchClient()
+    {
+        Console.WriteLine("Search a client by name: ");
+		string name = Console.ReadLine().ToLower();
+
+		foreach(Client client in clients)
+        {
+			if (client.Name == name)
+            {
+				Console.WriteLine($"Found! Client: {client.Name} {client.Surname}");
+            }
+            else
+            {
+				Console.WriteLine("Name not found!");
+				Thread.Sleep(1000);
+				UI.Home();
+            }
+        }
+		return null;
+    }
+
+    public List<Client> GetClients()
+    {
+        return clients;
+    }
+
+    public Client NewClient(List<Client> clients)
     {
         Console.WriteLine("Insert your name: ");
-		string name = Console.ReadLine();
+		string name = Console.ReadLine().ToLower();
 
 		Console.WriteLine("Insert your surname: ");
-		string surname = Console.ReadLine();
+		string surname = Console.ReadLine().ToLower();
 
 		Console.WriteLine("Insert your fiscal code: ");
-		string cf = Console.ReadLine();
+		string cf = Console.ReadLine().ToLower();
 
 		Console.WriteLine("Insert your salary: ");
 		int salary = Int32.Parse(Console.ReadLine());
@@ -48,5 +84,41 @@ public class Bank
 
 		return newClient;
     }
+
+	public void ModifyClient()
+    {
+		Console.WriteLine("Insert your fiscal code: ");
+		string cf = Console.ReadLine().ToLower();
+
+		foreach (Client client in clients)
+		{
+			if (client.CF == cf)
+			{
+				Console.WriteLine($"Found: {client.Name} {client.Surname}! Fiscal Code: {client.CF}, salary: {client.Salary}");
+
+				Console.WriteLine("What you want modify?");
+
+				Console.WriteLine("Insert your NEW name: ");
+				string name = Console.ReadLine().ToLower();
+
+				Console.WriteLine("Insert your NEW surname: ");
+				string surname = Console.ReadLine().ToLower();
+
+				Console.WriteLine("Insert your NEW fiscal code: ");
+				string fiscalCode = Console.ReadLine().ToLower();
+
+				Console.WriteLine("Insert your NEW salary: ");
+				int salary = Int32.Parse(Console.ReadLine());
+
+				Client modClient = new Client(name, surname, cf, salary);
+
+				clients.Add(modClient);
+			}
+			else
+			{
+                Console.WriteLine("Wrong fiscal code!");
+			}
+		}
+	}
 
 }
