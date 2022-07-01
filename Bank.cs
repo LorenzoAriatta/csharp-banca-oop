3f3efcebfab36a403fb8d93c27e872182b1003d5
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-01 14:37:17 +0200

    add Lists and logic for saving new Clients

---
+++ Bank.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Bank
+{
+	public string Name { get; set; }
+
+	List<Client> clients;
+	List<Lending> lendings;
+	public Bank(string name)
+	{
+		this.Name = name;
+	}
+
+	public Client NewClient()
+    {
+        Console.WriteLine("Insert your name: ");
+		string name = Console.ReadLine();
+
+		Console.WriteLine("Insert your surname: ");
+		string surname = Console.ReadLine();
+
+		Console.WriteLine("Insert your fiscal code: ");
+		string cf = Console.ReadLine();
+
+		Console.WriteLine("Insert your salary: ");
+		int salary = Int32.Parse(Console.ReadLine());
+
+		Client newClient = new Client(name, surname, cf, salary);
+
+        Console.WriteLine("Please wait, we're saving your data....");
+
+		foreach (Client client in clients)
+        {
+			if(client.CF == newClient.CF)
+            {
+                Console.WriteLine("Your data already exist!");
+				break;
+            }
+			else
+            {
+				clients.Add(newClient);
+			}
+        }
+		Thread.Sleep(3000);
+		Console.Clear();
+        Console.WriteLine("Success!");
+
+
+		return newClient;
+    }
+
+}
