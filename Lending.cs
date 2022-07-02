73733ee134eb666ab568271aa65c78a8434bbebf
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-02 14:28:31 +0200

    fix constructor and DateType

--- Lending.cs
+++ Lending.cs
@@ -2,14 +2,27 @@
 
 public class Lending
 {
-	public string ID { get; set; }
+	public int ID { get; set; }
+
+	public int Cash { get; set; }
 
 	Client lender;
 	public int Installment { get; set; }
-	DateTime StartLend { get; set; }
-	DateTime EndLend { get; set; }
+	public DateOnly StartLend { get; set; }
+	public DateOnly EndLend { get; set; }
 
-	public Lending()
+	public Lending(Client lender, int cash, int installment, DateOnly StartLend, DateOnly EndLend, int ID)
 	{
+		this.lender = lender;
+		this.Cash = cash;
+		this.Installment = installment;
+		this.StartLend = StartLend;
+		this.EndLend = EndLend;
+		this.ID = ID;
 	}
+
+	public Client GetClient()
+    {
+		return this.lender;
+    }
 }
