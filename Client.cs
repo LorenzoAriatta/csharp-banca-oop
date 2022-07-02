689a6072d7e678630edd62034dfe534743b352d6
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-02 14:27:59 +0200

    add second constructor

--- Client.cs
+++ Client.cs
@@ -6,6 +6,16 @@ public class Client
 	public string Surname { get; set; }
 	public string CF { get; set; }
 	public int Salary { get; set; }
+	public int TotalLendings { get; set; }
+
+	public Client(string name, string surname, string cf, int salary, int totalLendings)
+	{
+		this.Name = name;
+		this.Surname = surname;
+		this.CF = cf;
+		this.Salary = salary;
+		this.TotalLendings = totalLendings;
+	}
 
 	public Client(string name, string surname, string cf, int salary)
 	{
